0fa294f4e8f83462e239627cd9a837c29cad3939
suXin <suxinjke@gmail.com>
2018-11-04 01:33:45 +0700

    calculate score on every +pick arg so you can't tamper with choice by adding new args

--- src/routines/pick.ts
+++ src/routines/pick.ts
@@ -46,6 +46,22 @@ export default ( params: {
             const formattedArray = array.map( choiceToFormat => choiceToFormat.toLowerCase().replace( filterRegex, '' ).trim() )
             return formattedArray.indexOf( choice.toLowerCase().replace( filterRegex, '' ).trim() ) === index
         } )
+        .map( choice => {
+            const choice_seed = (
+                discordChannelRegex.test( choice ) || discordMentionRegex.test( choice ) ? choice :
+                choice.replace( filterRegex, '' )
+            )
+            .toLowerCase()
+            .trim()
+
+            rand.seed( choice_seed + additionalSeed )
+
+            return {
+                answer: choice,
+                score: rand.floatBetween( 0, 1 ) as number
+            }
+        } )
+        .sort( ( a, b ) => b.score - a.score )
 
     if ( choices.length <= 1 ) {
         const scold = lodash.sample( [ 'idiot', 'cunt', 'dingus', 'dumbass', 'asshole', 'dumbfuck' ] )
@@ -57,23 +73,7 @@ export default ( params: {
         ] )
     }
 
-    const seed = choices
-        .map( choice => {
-            if (
-                discordChannelRegex.test( choice ) ||
-                discordMentionRegex.test( choice )
-            ) {
-                return choice
-            }
+    const answer = choices[0].answer
 
-            return choice.replace( filterRegex, '' )
-        } )
-        .join( ' ' )
-        .toLowerCase()
-        .trim()
-
-    rand.seed( seed + additionalSeed )
-
-    const answer = choices[ rand( choices.length ) ]
     return randomMarkdownDecorate ? randomMarkdownDecorateFunction( answer ) : answer
 }
